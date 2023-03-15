8d6f65cc32eede112c42e824c17d7d5d99ac6a82
Ian Reuven Feldman <irfeldma@calpoly.edu>
2023-03-15 15:53:19 -0700

    Define characters

--- sw_src/assembly_code/font-demo-test.s
+++ sw_src/assembly_code/font-demo-test.s
@@ -2,9 +2,37 @@
 .eqv VG_ADDR, 0x11000120
 .eqv VG_COLOR, 0x11000140
 
-.eqv LETTER_A, 0x000069f9
-.eqv LETTER_B, 0x0000ef9e
-.eqv LETTER_C, 0x00006987
+.eqv CHAR_A, 		0x0000f9f9
+.eqv CHAR_B, 		0x0000ef9f
+.eqv CHAR_C, 		0x0000f88f
+.eqv CHAR_D, 		0x0000e99e
+.eqv CHAR_E, 		0x0000fe8f
+.eqv CHAR_F, 		0x0000f8e8
+.eqv CHAR_G, 		0x0000f89f
+.eqv CHAR_H, 		0x000099f9
+.eqv CHAR_I, 		0x0000e44e
+.eqv CHAR_J, 		0x000072ae
+
+.eqv CHAR_0, 		0x0000fb9f
+.eqv CHAR_1, 		0x0000c44e
+.eqv CHAR_2,		0x0000f3cf
+.eqv CHAR_3,		0x0000f71f
+.eqv CHAR_4,		0x000099f1
+.eqv CHAR_5,		0x0000f9f7
+.eqv CHAR_6,		0x00008f9f
+.eqv CHAR_7,		0x0000f111
+.eqv CHAR_8,		0x0000f9ff
+.eqv CHAR_9,		0x0000f9f1
+
+.eqv CHAR_PLUS, 	0x000004e4
+.eqv CHAR_MINUS, 	0x000000e0
+.eqv CHAR_MULT		0X00000a4a
+.eqv CHAR_DIV,		0X00002448
+.eqv CHAR_SQRT, 	0x000074d4
+.eqv CHAR_EQUALS,	0x00000e0e
+.eqv CHAR_DOT, 		0x00000004
+.eqv CHAR_OPENPAREN, 	0x00002442
+.eqv CHAR_CLOSEPAREN,	0x00004224
 
 .text
 main:
@@ -17,21 +45,21 @@ main:
 	
 	li a0, 33		# X coordinate
 	li a1, 25		# Y coordinate
-	li a2, LETTER_A
+	li a2, CHAR_A
 	li a3, 0xE0		# color red 
-	call draw_letter
+	call draw_char
 	
 	li a0, 38		# X coordinate
 	li a1, 25		# Y coordinate
-	li a2, LETTER_B
+	li a2, CHAR_B
 	li a3, 0x1c		# color green
-	call draw_letter
+	call draw_char
 	
 	li a0, 43		# X coordinate
 	li a1, 25		# Y coordinate
-	li a2, LETTER_C
+	li a2, CHAR_C
 	li a3, 0x03		# color blue
-	call draw_letter
+	call draw_char
 
     	# li a0, 10		# X coordinate
 	# li a1, 20		# Y coordinate
@@ -62,9 +90,9 @@ main:
 
 done:	j done # continuous loop
 
-# draws a letter (a2: letter halfword) at (a0, a1)
+# draws a char (a2: char halfword) at (a0, a1)
 # modifies t0-t6, a0, a1
-draw_letter:
+draw_char:
 	addi sp, sp, -4	# store ra on stack
 	sw ra, 0(sp)
 	mv t0, a2	# move letter into t0
@@ -73,9 +101,9 @@ draw_letter:
 	li t3, 0 	# load y counter into t3
 	li t4, 3	# counters increment when they reach 3 (t4)
 	mv t5, a0	# save x coord to t5
-draw_let1:
+draw_char1:
 	and t6, t0, t1		# letter && bitmask to get just first bit
-	beqz t6, draw_let2 	# don't draw anything if pixel is 0 (branch to draw_let2)
+	beqz t6, draw_char2 	# don't draw anything if pixel is 0 (branch to draw_let2)
 	# draw_dot modifies t0, t1, so they must be pushed to stack:
 	addi sp, sp, -4
 	sw t0, 0(sp)
@@ -87,18 +115,18 @@ draw_let1:
 	addi sp, sp, 4
 	lw t0, 0(sp)
 	addi sp, sp, 4
-draw_let2:
+draw_char2:
 	srli t1, t1, 1		# shift bitmask to right 1
-	beq t2, t4, draw_let3	# branch if x counter has reached end
+	beq t2, t4, draw_char3	# branch if x counter has reached end
 	addi t2, t2, 1		# increment x counter if not
 	addi a0, a0, 1		# and increment x coord
-	j draw_let1		# loop back
-draw_let3:
+	j draw_char1		# loop back
+draw_char3:
 	add t2, x0, x0		# reset x counter
 	mv a0, t5		# reset x coord
 	addi t3, t3, 1		# increment y counter
 	addi a1, a1, 1		# increment y coord
-	ble t3, t4, draw_let1   # loop back if y counter has not reached end (4)
+	ble t3, t4, draw_char1  # loop back if y counter has not reached end (4)
 	lw ra, 0(sp)		# restore ra from stack
 	addi sp, sp, 4
 	ret
